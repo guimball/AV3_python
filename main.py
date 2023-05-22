d5dfdd92c1439e309c457b5ca695ad3b0350ec39
André Luiz <124224711+guimball@users.noreply.github.com>
2023-05-22 07:33:19 -0300

    Update main.py
    agora funcionando apenas puxando informações da tag H3, falta puxar informações das páginas referenciadas e realizar a contagem das mesmas

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
 
-class WebScraper:
-    def __init__(self, url):
-        self.url = url
-        self.data = []
 
-    def scrape(self):
-        response = requests.get(self.url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        # Encontre aqui os elementos HTML que deseja coletar os dados
-        for element in soup.find_all('div', class_='example'):
-            data_point = element.get_text()
-            self.data.append(data_point)
+class Parser:
 
-    def print_data(self):
-        for data_point in self.data:
-            print(data_point)
+  def parse(self, html):
+    raise NotImplementedError
+
+
+class DataExtractor:
+
+  def extract(self, soup):
+    raise NotImplementedError
+
+
+class BeautifulSoupParser(Parser):
+
+  def parse(self, html):
+    return BeautifulSoup(html, 'html.parser')
+
+
+class CustomDataExtractor(DataExtractor):
+
+  def extract(self, soup):
+    data = []
+    h3_tags = soup.find_all('h3')
+    for tag in h3_tags:
+      data.append(tag.text)
+    return data
+
+
+class Scraper:
+
+  def __init__(self, parser, data_extractor):
+    self.parser = parser
+    self.data_extractor = data_extractor
+
+  def scrape(self, url):
+    response = requests.get(url)
+    if response.status_code == 200:
+      html = response.text
+      soup = self.parser.parse(html)
+      data = self.data_extractor.extract(soup)
+      return data
+    else:
+      print(f"Erro ao acessar a URL: {response.status_code}")
+      return None
+
 
 # Exemplo de uso
-url = 'https://www.example.com'
-scraper = WebScraper(url)
-scraper.scrape()
-scraper.print_data()
+if __name__ == "__main__":
+  parser = BeautifulSoupParser()
+  data_extractor = CustomDataExtractor()
+  scraper = Scraper(parser, data_extractor)
+
+  url = "https://guimball.github.io/projectCecilia/"
+  data = scraper.scrape(url)
+  print(data)
